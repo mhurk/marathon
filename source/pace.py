cc9d6fa9b3efa92886cd3590ffdd2e6dead652ca
mhurk <149044252+mhurk@users.noreply.github.com>
2024-09-07 15:05:36 +0200

    update

--- source/pace.py
+++ source/pace.py
@@ -19,24 +19,6 @@ runners_df = pd.DataFrame({
     'Pace': random_paces  # Pace in minutes per mile
 })
 
-# # Simulate distance covered by each runner at a given time (e.g., 3 hours into the race)
-# time_elapsed = simulation_time  # in minutes
-# runners_df['DistanceCovered'] = time_elapsed / runners_df['Pace']  # Distance in miles
-
-# # Cap distances at the total race distance (26.2 miles)
-# runners_df['DistanceCovered'] = np.where(runners_df['DistanceCovered'] > race_distance, race_distance, runners_df['DistanceCovered'])
-
-# # Visualize distribution of runners along the course
-# plt.figure(figsize=(10, 6))
-# plt.hist(runners_df['DistanceCovered'], bins=30, color='blue', alpha=0.7)
-# plt.title(f'Distribution of Runners after {simulation_time} minutes')
-# plt.xlabel('Distance Covered (km)')
-# plt.ylabel('Number of Runners')
-# plt.grid(True)
-# plt.show()
-
-
-
 # Prepare figure
 fig, ax = plt.subplots(figsize=(10, 6))
 
@@ -63,4 +45,4 @@ ani = animation.FuncAnimation(fig, update, frames=len(time_steps), repeat=False)
 ani.save('.\images\marathon_simulation_animation.mp4', writer='ffmpeg', fps=10)
 
 # To show the animation in a window
-plt.show()
+#plt.show()
