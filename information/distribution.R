88fcb02e2d00d1d499a64a77c37612fd22c9f93e
mhurk <149044252+mhurk@users.noreply.github.com>
2024-09-09 23:35:52 +0200

    distro

---
+++ information/distribution.R
@@ -0,0 +1,34 @@
+library(fitdistrplus)
+library(lubridate)
+
+setwd("//BigStation/data/Programming/Marathon_pace/information")
+
+results <- readxl::read_excel("Results-marathon-eindhoven-2017.xlsx")
+
+ChipTime <- parse_date_time(results$Chiptime, orders = "%Y-%m-%d %H:%M:%S")
+ChipMinute <- hour(ChipTime)*60 + minute(ChipTime) + second(ChipTime)/60
+
+descdist(GunMinute, discrete = FALSE)
+
+hist(ChipMinute, breaks=100)
+
+# Comparison of various fits 
+fitW <- fitdist(ChipMinute, "weibull")
+fitg <- fitdist(ChipMinute, "gamma")
+fitln <- fitdist(ChipMinute, "lnorm")
+fitnorm <- fitdist(ChipMinute, "norm")
+summary(fitW)
+summary(fitg)
+summary(fitln)
+summary(fitnorm)
+cdfcomp(list(fitW, fitg, fitln, fitnorm), legendtext=c("Weibull", "gamma", "lognormal", "normal"))
+denscomp(list(fitW, fitg, fitln, fitnorm), legendtext=c("Weibull", "gamma", "lognormal", "normal"))
+qqcomp(list(fitW, fitg, fitln, fitnorm), legendtext=c("Weibull", "gamma", "lognormal", "normal"))
+ppcomp(list(fitW, fitg, fitln, fitnorm), legendtext=c("Weibull", "gamma", "lognormal", "normal"))
+gofstat(list(fitW, fitg, fitln, fitnorm), fitnames=c("Weibull", "gamma", "lognormal", "normal"))
+
+#conclusion: Gamma fits best (lowest number)
+
+# The gamma distribution has the lowest values across all goodness-of-fit statistics (KS, CvM, AD) 
+# and criteria (AIC, BIC), suggesting it is the best fit for your data compared to the Weibull and lognormal distributions.
+
