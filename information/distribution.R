c834033556afbbd2ba7e8a6ad58b630e3e553e61
mhurk <149044252+mhurk@users.noreply.github.com>
2024-09-10 07:59:00 +0200

    update

--- information/distribution.R
+++ information/distribution.R
@@ -1,12 +1,14 @@
 library(fitdistrplus)
 library(lubridate)
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 results <- readxl::read_excel("Results-marathon-eindhoven-2017.xlsx")
 
 ChipTime <- parse_date_time(results$Chiptime, orders = "%Y-%m-%d %H:%M:%S")
 ChipMinute <- hour(ChipTime)*60 + minute(ChipTime) + second(ChipTime)/60
 
-descdist(GunMinute, discrete = FALSE)
+descdist(ChipMinute, discrete = FALSE)
 
 hist(ChipMinute, breaks=100)
 
@@ -28,11 +30,14 @@ gofstat(list(fitW, fitg, fitln, fitnorm), fitnames=c("Weibull", "gamma", "lognor
 # The gamma distribution has the lowest values across all goodness-of-fit statistics (KS, CvM, AD) 
 # and criteria (AIC, BIC), suggesting it is the best fit for your data compared to the Weibull and lognormal distributions.
 
-meanPace <- mean(ChipMinute)
-sdPace <- sd(ChipMinute)
+meanTime <- mean(ChipMinute)
+sdTime <- sd(ChipMinute)
 
-paste("mean pace:", sprintf("%.02d:%.02d:%.02d", meanPace %/% 60, round(meanPace %% 60), round(meanPace%%1*60)), sep = " ")
-paste("standard deviation:", sprintf("%.02d:%.02d:%.02d", sdPace %/% 60, round(sdPace %% 60), round(sdPace%%1*60)), sep = " ")
+paste("mean time:", sprintf("%.02d:%.02d:%.02d", meanTime %/% 60, round(meanTime %% 60), round(meanTime%%1*60)), sep = " ")
+paste("standard deviation:", sprintf("%.02d:%.02d:%.02d", sdTime %/% 60, round(sdTime %% 60), round(sdTime%%1*60)), sep = " ")
 
 
-
+meanPace <- meanTime/42.195 #input for simulation
+sdPace <- sdTime/42.195 #input for simulation
+paste("mean pace :", sprintf("%.02d:%.02d", round(meanPace %% 60), round(meanPace%%1*60)), sep = " ")
+paste("standard deviation:", sprintf("%.02d:%.02d", round(sdPace %% 60), round(sdPace%%1*60)), sep = " ")
